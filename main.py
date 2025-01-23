29d57bf23ce2e6afdd2547fa6bb9526a23db495d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-23 20:00:51 +0900

    Creating start and help functions

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # Telegram AI Bot using Python and Tensorflow
-
-
-
+from telegram.ext import *
+from io import BytesIO
+import cv2
+import numpy as np 
+import tensorflow as tf 
 
 with open("token.txt", "r") as f:
     TOKEN = f.read()
+
+def start(update, context):
+    update.message.reply_text("Welcome to the Telegram Bot!")
+
+def help(update, context):
+    update.message.reply_text("""
+    /start - Starts the conversation
+    /help - Shows this message
+    /train - trains the neural network
+
+    """)
