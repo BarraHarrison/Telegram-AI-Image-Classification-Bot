a4ae99ea76b07cebcc9492525fa6c94fb043a628
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-25 19:31:12 +0900

    The rest of the layers added to the model

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ class_names = ["Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "S
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Conv2D(32, (3,3), activation="relu", input_shape=(32, 32, 3)))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation="relu"))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64, activation="relu"))
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
 
 
 def start(update, context):
