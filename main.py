f04f910a40f20f731a59d8c5933c503445c0df41
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-25 19:39:47 +0900

    Saving the model and informing the user

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ def train(update, context):
     update.message.reply_text("Model is being trained...")
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     model.fit(x_train, y_train, epochs=10, validation_data=[x_test, y_test])
+    model.save("cifar_classifier.model")
+    update.message.reply_text("Done! You can now send a photo if you wish.")
     
 
 def handle_message(update, context):
